d740cd4ad83ef90444e987fa930d709ab519ef9d
Burhan Bisgin <burhan.bb@hotmail.com>
2026-02-20 15:22:05 +0300

    Initial Commit

---
+++ SubLogic/SubLogicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubLogicApp.swift
+//  SubLogic
+//
+//  Created by Burhan Bişgin on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SubLogicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
